184d5a60394dbd07872b58bfb1f615e80049f389
Peter Kranz <peter@myref.net>
2021-03-29 03:02:15 +0200

    init a new vue project

--- src/main.js
+++ src/main.js
@@ -1,32 +1,4 @@
-const { app, BrowserView, BrowserWindow } = require('electron')
-const path = require('path')
+import { createApp } from "vue";
+import App from "./App.vue";
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 1000,
-    height: 500,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      webviewTag: true
-    }
-  })
-
-  win.loadFile('web/index.html')
-}
-
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
+createApp(App).mount("#app");
